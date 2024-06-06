23c5e81c510920b9eaf6d9df650914080820a7ba
Igor Kawa <igorkawa99@gmail.com>
2024-06-06 23:34:08 +0200

    initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/Controller/MainController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    #[Route('/match-analysis', name: 'match_analysis')]
+    public function matchAnalysis(): Response
+    {
+        return $this->render('main/match_analysis.html.twig');
+    }
+
+    #[Route('/player-training', name: 'player_analysis')]
+    public function playerTraining(): Response
+    {
+        return $this->render('main/player_analysis.html.twig');
+    }
+
+    #[Route('/news', name: 'news')]
+    public function news(): Response
+    {
+        return $this->render('main/news.html.twig');
+    }
+}
+
